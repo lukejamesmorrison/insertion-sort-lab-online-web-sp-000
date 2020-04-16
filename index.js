b84e32803b96bae26b5cf97342c10cc0856ce091
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-16 21:53:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function findMinAndRemove(array){
+let array = [2, 4, 12, 3, 1];
 
+function findMinAndRemove(array){
+  var min = array[0];
+  var minIndex = 0;
+
+  for(let i = 0; i < array.length; i++)
+  {
+    if(array[i] < min)
+    {
+      min = array[i];
+      minIndex = i;
+    };
+  };
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
+  var sorted = [];
 
+  while(array.length > 0)
+  {
+    sorted.push(findMinAndRemove(array));
+  };
+
+  return sorted;
 }

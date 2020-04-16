43fbb9894d6a6f3676af3577c626fc0e3f52211c
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-16 21:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ console.log(findMinAndRemove(initArray));
 
 function findMinAndRemove(array){
   var min = array[0];
+  
   for(let i = 0; i < array.length; i++)
   {
     if(array[i] < min){
       min = array[i];
     };
-  }
-
+  };
   return min;
 }
 

08c1e4b6d9bdd90a6a7d92e3ab08a3f6eb6bbd51
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-16 21:41:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+let array = [1, 4, 12, 3]
+
+console.log(findMinAndRemove(array));
+
 function findMinAndRemove(array){
 
   var min = array[0];
+  var minIndex = 0;
 
   for(let i = 0; i < array.length; i++)
   {
     if(array[i] < min){
-      
-    }
+      min = array[i];
+      minIndex = i;
+    };
   }
 
   // if index is less that next index, remove and set as min
